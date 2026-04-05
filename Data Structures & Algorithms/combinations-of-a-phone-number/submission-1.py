69152d6b3043db7ebaacb4f2adc3bce78c7c157b
Omar Choudhry <71817542+omariosc@users.noreply.github.com>
2026-04-05 20:35:09 +0100

    Add: combinations-of-a-phone-number - submission-1

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-1.py
@@ -0,0 +1,33 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+
+        letters = {
+            "2": ["a", "b", "c"],
+            "3": ["d", "e", "f"],
+            "4": ["g", "h", "i"],
+            "5": ["j", "k", "l"],
+            "6": ["m", "n", "o"],
+            "7": ["p", "q", "r", "s"],
+            "8": ["t", "u", "v"],
+            "9": ["w", "x", "y", "z"],
+        }
+        presses = list(digits)
+        length = len(presses)
+
+        combinations = []
+
+        def computeCombinations(i: int, curr: List[str]) -> None:
+            if i == length:
+                combinations.append(curr.copy())
+                return
+                
+            for ch in letters[presses[i]]:
+                curr.append(ch)
+                computeCombinations(i+1, curr)
+                curr.pop()
+
+        computeCombinations(0, [])
+
+        return ["".join(i) for i in combinations]
