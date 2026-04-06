f286609b203dcde14be5595c3a3113cad0bf1f54
Omar Choudhry <71817542+omariosc@users.noreply.github.com>
2026-04-06 16:19:09 +0100

    Add: partition-equal-subset-sum - submission-4

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-4.py
@@ -0,0 +1,20 @@
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        total = sum(nums)
+        if total % 2 == 1:
+            return False
+
+        ROWS, COLS = len(nums) + 1, total // 2 + 1
+        dp = [[0] * COLS for _ in range(ROWS)]
+
+        for i in range(1, ROWS):
+            for j in range(1, COLS):
+                if j >= nums[i-1]:
+                    dp[i][j] = max(dp[i-1][j], nums[i-1] + dp[i-1][j-nums[i-1]])
+                else:
+                    dp[i][j] = dp[i-1][j]
+                
+                if dp[i][j] == COLS - 1:
+                    return True
+
+        return False
