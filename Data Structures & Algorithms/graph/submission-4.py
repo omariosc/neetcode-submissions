2b35c55011f8ff3887ec8ea10ed35f3277ada0de
Omar Choudhry <71817542+omariosc@users.noreply.github.com>
2026-05-03 01:17:58 +0100

    Add: graph - submission-4

---
+++ Data Structures & Algorithms/graph/submission-4.py
@@ -0,0 +1,35 @@
+class Graph:
+    
+    def __init__(self):
+        self.graph = {}
+
+    def addEdge(self, src: int, dst: int) -> None:
+        if src not in self.graph:
+            self.graph[src] = []
+        if dst not in self.graph:    
+            self.graph[dst] = []
+        self.graph[src].append(dst)
+
+    def removeEdge(self, src: int, dst: int) -> bool:
+        if src not in self.graph or dst not in self.graph:
+            return False
+        
+        self.graph[src].remove(dst)
+        return True
+
+    def hasPath(self, src: int, dst: int) -> bool:
+        if src not in self.graph or dst not in self.graph:
+            return False
+
+        q = collections.deque()
+        visited = set()
+        q.append(src)
+        while q:
+            node = q.popleft()
+            if node == dst:
+                return True
+            for neighbour in self.graph[node]:
+                if not neighbour in visited:
+                    q.append(neighbour)
+                    visited.add(neighbour)
+        return False
