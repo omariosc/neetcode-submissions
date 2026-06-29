7f832041ff0f8e7d90faaa696d1d3a6db49b88de
Omar Choudhry <71817542+omariosc@users.noreply.github.com>
2026-06-29 17:33:11 +0100

    Add: missing-number - submission-5

---
+++ Data Structures & Algorithms/missing-number/submission-5.py
@@ -0,0 +1,10 @@
+from functools import reduce
+from operator import ixor
+
+class Solution:
+    def missingNumber(self, nums: list[int]) -> int:
+        return reduce(
+            lambda acc, i: acc ^ i ^ nums[i], 
+            range(len(nums)), 
+            len(nums)
+        )
