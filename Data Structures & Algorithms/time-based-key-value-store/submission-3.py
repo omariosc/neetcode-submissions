fc0b72fe3cb8002ee9547e96bd33b7b53c80d4f8
Omar Choudhry <71817542+omariosc@users.noreply.github.com>
2026-06-29 18:19:46 +0100

    Add: time-based-key-value-store - submission-3

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-3.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+import bisect
+
+class TimeMap:
+
+    def __init__(self):
+        self.keys = defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.keys[key].append([timestamp, value])
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.keys:
+            return ""
+        idx = bisect.bisect_right(self.keys[key], [timestamp, chr(127)])
+        return self.keys[key][idx - 1][1] if idx > 0 else ""
+        # start, end = 0, len(values) - 1
+        # while start <= end:
+        #     mid = (start + end) // 2
+        #     if timestamp < values[mid][0]:
+        #         end = mid - 1
+        #     elif timestamp > values[mid][0]:
+        #         start = mid + 1
+        #     else:
+        #         return values[mid][1]
+        # return values[end][1] if end >= 0 else ""
